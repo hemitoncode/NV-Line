2d5a592a6e5a43a2e45da9a3b29e9e356f1b4456
Hemit Patel <hemitvpatel@gmail.com>
2026-02-25 21:25:35 -0500

    allow for extensiability with new cities (now seeding toronto)

--- cities/new-york.py
+++ cities/new-york.py
@@ -1,24 +1,25 @@
-import time 
+import time
 import asyncio
 
-from .helper.newyorkScraper import scrapeCouncilMeetings, scrapeMeetingDetails, scrapeLegislationDetail
-from .helper.senderServices.emailService import sendEmails
-from .helper.senderServices.htmlReportPackage import buildHTMLReport
-from .helper.webRequester import fetchCouncilMeetings, fetchMeetingDetails, fetchLegislationDetails
-from .helper.ai.aiProcessor import processBillsWithAI
+from .new_york_helpers.newyorkScraper import scrapeCouncilMeetings, scrapeMeetingDetails, scrapeLegislationDetail
+from ..helper.senderServices.emailService import sendEmails
+from ..helper.senderServices.htmlReportPackage import buildHTMLReport
+from .new_york_helpers.webRequester import fetchCouncilMeetings, fetchMeetingDetails, fetchLegislationDetails
+from ..helper.ai.aiProcessor import processBillsWithAI
+
 
 async def cli():
     start_time = time.time()
-    
+
     print("Fetching meetings...")
     meetingHTML = await fetchCouncilMeetings()
-    
+
     print("Scraping meetings...")
     scrapeCouncilMeetings(meetingHTML)
-    
+
     print("Fetching meeting details (legislation list)...")
     await fetchMeetingDetails()
-    
+
     print("Scraping meeting details (legislation list)...")
     scrapeMeetingDetails()
 
@@ -27,21 +28,23 @@ async def cli():
 
     print("Scraping legislation details...")
     scrapeLegislationDetail()
-    
+
     print("Processing bills with AI...")
-    await processBillsWithAI() 
+    await processBillsWithAI()
 
     print("Compile HTML report...")
     buildHTMLReport()
-    
+
     print("Sending emails...")
     sendEmails()
-    
+
     elapsed_time = time.time() - start_time
     print(f"Total time: {elapsed_time:.2f} seconds")
 
+
 def main():
     asyncio.run(cli())
+
 
 if __name__ == "__main__":
     main()
